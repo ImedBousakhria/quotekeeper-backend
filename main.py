e85b396046b916bc1689e488cc42a1a4b23b5edd
Imed Bousakhria <i_bousakhria@estin.dz>
2024-02-23 21:41:00 +0100

    fix: import author router

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from routers.book import routerBook
 from routers.quote import routerQuote
 from routers.tag import routerTag
 from routers.user import routerUser
-from routers.fav import routerFav
+from routers.author import routerAuthor
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -24,7 +24,7 @@ app.add_middleware(
 app.include_router(routerBook)
 app.include_router(routerUser)
 app.include_router(routerTag)
-# app.include_router(routerFav)
+app.include_router(routerAuthor)
 app.include_router(routerQuote)
 
 
