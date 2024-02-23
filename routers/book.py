be04b6323cd3f6659e7720eff5d7350d4ea8fef0
Imed Bousakhria <i_bousakhria@estin.dz>
2024-02-23 18:43:13 +0100

    feat: added Book update, get 1, delete

--- routers/book.py
+++ routers/book.py
@@ -22,14 +22,18 @@ def get_db():
 async def show_books(db:Session=Depends(get_db)):
     return crud.show_books(db)
 
+@routerBook.get('/book')
+async def get_book(book_id: int, db:Session=Depends(get_db)):
+    return crud.get_book(db, book_id)
+
 @routerBook.post('/add_book')
 async def create_book(book:schemas.BookCreate, db:Session=Depends(get_db)):
     return crud.create_book(db, book)
 
 @routerBook.put('/update_book')
-async def update_book():
-    pass
+async def update_book(book:schemas.BookCreate, id_book:int, db:Session=Depends(get_db)):
+    return crud.update_book(db, id_book, book )
 
 @routerBook.delete('/delete_book')
-async def delete_book():
-    pass
+async def delete_book(id_book:int, db:Session=Depends(get_db)):
+    return crud.delete_book(db, id_book)
