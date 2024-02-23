921602f40a2ace8fc6ae1a561a7d9126d90e65c1
Imed Bousakhria <i_bousakhria@estin.dz>
2024-02-23 18:41:56 +0100

    feat: Quote CRUDs

--- routers/quote.py
+++ routers/quote.py
@@ -19,17 +19,29 @@ def get_db():
         db.close()
         
 @routerQuote.get('/all_quotes')
-async def show_quotes():
-    pass
+async def get_quotes(db:Session=Depends(get_db)):
+    return crud.get_quotes(db)
+
+@routerQuote.get('/quote')
+async def get_quote(quote_id: int, db:Session=Depends(get_db)):
+    return crud.get_quote(db, quote_id)
+
+@routerQuote.get('/quotes_by_book')
+async def get_quotes_by_book(book_id: int, db:Session=Depends(get_db)):
+    return crud.get_quotes_by_book(db, book_id)
+
+@routerQuote.get('/quotes_by_user')
+async def get_quotes_by_user(user_id: int, db:Session=Depends(get_db)):
+    return crud.get_quotes_by_user(db, user_id)
 
 @routerQuote.post('/add_quote')
-async def create_quote():
-    pass
+async def create_quote(quote:schemas.QuoteCreate, db:Session=Depends(get_db)):
+    return crud.create_quote(db, quote)
 
 @routerQuote.put('/update_quote')
-async def update_quote():
-    pass
+async def update_quote(quote_id:int, quote_body: schemas.QuoteCreate, db:Session=Depends(get_db)):
+    return crud.update_quote(db, quote_id, quote_body)
 
 @routerQuote.delete('/delete_quote')
-async def delete_quote():
-    pass
+async def delete_quote(quote_id:int, db:Session=Depends(get_db)):
+    return crud.delete_quote(db, quote_id)
