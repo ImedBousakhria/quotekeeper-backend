8db3c5301823dc125c43ce1e849b7a0a239bba27
Imed Bousakhria <i_bousakhria@estin.dz>
2024-02-23 18:42:40 +0100

    feat: Tag CRUDs

--- routers/tag.py
+++ routers/tag.py
@@ -19,5 +19,21 @@ def get_db():
         db.close()
         
 @routerTag.get('/all_tags')
-async def show_tags():
-    pass
+async def get_tags(db:Session=Depends(get_db)):
+    return crud.get_tags(db)
+
+@routerTag.get('/tag')
+async def get_tag(tag_id: int, db:Session=Depends(get_db)):
+    return crud.get_tag(db, tag_id)
+
+@routerTag.post('/add_tag')
+async def create_tag(tag:schemas.TagCreate, db:Session=Depends(get_db)):
+    return crud.create_tag(db, tag)
+
+@routerTag.put('/update_tag')
+async def update_tag(tag_id:int, tag_body: schemas.TagCreate, db:Session=Depends(get_db)):
+    return crud.update_tag(db, tag_id, tag_body)
+
+@routerTag.delete('/delete_tag')
+async def delete_tag(tag_id:int, db:Session=Depends(get_db)):
+    return crud.delete_tag(db, tag_id)
